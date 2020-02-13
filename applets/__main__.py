7088247a7a735d6ebcaf9585d0dbfbdd8771fd3a
allthroughthenight <dubmore@gmail.com>
2020-02-13 13:16:52 -0700

    applets

--- applets/__main__.py
+++ applets/__main__.py
@@ -1,6 +1,20 @@
-from modules import mastery as mastery
+from modules import budget as budget
+# from modules import ip_addr as ip_addr
 from modules import life_clock as life_clock
+from modules import mastery as mastery
+# from modules import ping as ping
 
 if (__name__ == '__main__'):
-    mastery.run()
-    life_clock.run()
+    while True:
+        print("Main Menu")
+        print("1: Budget")
+        print("2: Life Clock")
+        print("3: Mastery")
+        choice = int(input("Enter Your Choice: "))
+
+        if choice == 1:
+            budget.run()
+        elif choice == 2:
+            life_clock.run()
+        elif choice == 3:
+            mastery.run()
